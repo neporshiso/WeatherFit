a86c29de73d3400fae319af4526fede669aa5835
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-10-01 15:32:04 -0400

    populating tmrw weather forecast. slight changes to today forecast

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-// ? So far using three APIs.  ZipCode to Lat/Lon API.  DarkSky for weather data.  Open Weather for weather icon.
-// Today's Weather Box:  Format Degrees.  Populate City, State as well.   Do Tomorrows weather.
+/* API Counter  
+
+1. public.opendatasoft.com API:  Take a zipcode. Give lat/lon and city/state.
+2. DarkSky API: Temperatures and Precipitation Chance
+3. Open Weather: Weather Icon
+4. [In Progress]: Unofficial ASOS for Clothing
+
+*/
 
 function get(url) {
     return fetch(url)
@@ -7,19 +13,23 @@ function get(url) {
         .then(data => data);
 }
 
-// When the zip code form is submitted, grab the zip code, convert it to coordinates and get the weather data
+// When the zip code form is submitted, grab the zip code, convert it to coordinates and get the weather data and icon
 zipcodeForm.addEventListener("submit", function(event) {
-    event.preventDefault(); // default behavior is to reload the page
+    event.preventDefault(); 
     let zipcodeValue = document.getElementById("zipcodeInput").value;
     convertZipToCoordinates(zipcodeValue)
     getIcon(zipcodeValue)
 })  
 
 function convertZipToCoordinates(zip) {
+    const location = document.getElementById("todayLocation");
     get(
         `https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=${zip}&facet=state&facet=timezone&facet=dst`
     ).then(function(response) {
         let coordinates = response.records[0].geometry.coordinates;
+        let city = response.records[0].fields.city
+        let state = response.records[0].fields.state
+        location.innerHTML = `${city}, ${state}`
         getWeather(coordinates);
     });
 }
@@ -47,12 +57,29 @@ function printOutfit (temp) {
 
 function getWeather(coordinates) {
     let tempHtml = document.getElementById("feelsLike")
+
+    let tmrTempHigh = document.getElementById("tmrTempHigh")
+    let tmrTempLow = document.getElementById("tmrTempLow")
+    let tmrRain= document.getElementById("tmrRainChance")
+
     let lat = coordinates[1];
     let lon = coordinates[0];
     let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/1387466109e308e8de851d6f09a87c39/${lat},${lon}`;
+
     get(url).then(function(response) {
+        // Storing appropriate values
         let apparentTemp = response.currently.apparentTemperature;
-        tempHtml.innerHTML = apparentTemp
+        let tmrwTempHighValue = response.daily.data[0].apparentTemperatureHigh
+        let tmrwTempLowValue = response.daily.data[0].apparentTemperatureLow
+        let tmrwRainChanceValue = response.daily.data[0].precipProbability * 100
+        
+        // Populate DOM with weather data
+        tempHtml.innerHTML = `${apparentTemp}&#176F`
+        tmrTempHigh.innerHTML = `${tmrwTempHighValue}&#176F`
+        tmrTempLow.innerHTML = `${tmrwTempLowValue}&#176F`
+        tmrRain.innerHTML = `${tmrwRainChanceValue}&#37;`
+
+        // Call printOutfit 
         // printOutfit(apparentTemp);
     });
 }
@@ -60,6 +87,7 @@ function getWeather(coordinates) {
 function getIcon(zip) {
     let u = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=c7d69379e4f97e2b0e42695dddb0ad27`;
     const iconImg = document.getElementById("weatherIcon");
+    
     get(u)
         .then(response => response)
         .then(function(data) {
@@ -76,6 +104,7 @@ function getIcon(zip) {
         });
 }
 
+// IIFE with default city Atlanta, GA
 (function(){
     convertZipToCoordinates(30342)
     getIcon(30342)

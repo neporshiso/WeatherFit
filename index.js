8242c93e561b1218dae4ef6ec9ea837be64e2f5e
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-10-04 14:34:31 -0400

    ran js file through prettier

--- index.js
+++ index.js
@@ -7,15 +7,19 @@
 
 */
 function getDate() {
-    var currentDate = new Date()
-    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var currentDate = new Date();
+    var options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    };
 
     let todayDate = document.getElementById("currentDate");
-    todayDate.innerHTML = new Intl.DateTimeFormat('en-US', options).format(currentDate);
+    todayDate.innerHTML = new Intl.DateTimeFormat("en-US", options).format(
+        currentDate
+    );
 }
-    
-
-
 
 function get(url) {
     return fetch(url)
@@ -23,35 +27,32 @@ function get(url) {
         .then(data => data);
 }
 
-
 modalBtn.addEventListener("click", function(e) {
     e.preventDefault();
-    modal.style.display = "block"
+    modal.style.display = "block";
 });
 
-closeBtn.onclick = function(){ 
-    modal.style.display = "none"
-  }
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+};
 
-window.onclick = function(e){
-    if(e.target == modal){
-      modal.style.display = "none"
+window.onclick = function(e) {
+    if (e.target == modal) {
+        modal.style.display = "none";
     }
-   }   
-
+};
 
 // When the zip code form is submitted, grab the zip code, convert it to coordinates and get the weather data and icon
 zipcodeForm.addEventListener("submit", function(event) {
-    event.preventDefault(); 
-    let clothesToday= document.getElementById("todayClothes");
+    event.preventDefault();
+    let clothesToday = document.getElementById("todayClothes");
     let clothesTomorrow = document.getElementById("tomorrowClothes");
     clothesTomorrow.innerHTML = "";
     clothesToday.innerHTML = "";
     let zipcodeValue = document.getElementById("zipcodeInput").value;
-    convertZipToCoordinates(zipcodeValue)
-    getIcon(zipcodeValue)
-})  
-
+    convertZipToCoordinates(zipcodeValue);
+    getIcon(zipcodeValue);
+});
 
 function convertZipToCoordinates(zip) {
     const location = document.getElementById("todayLocation");
@@ -59,27 +60,51 @@ function convertZipToCoordinates(zip) {
         `https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=${zip}&facet=state&facet=timezone&facet=dst`
     ).then(function(response) {
         let coordinates = response.records[0].geometry.coordinates;
-        let city = response.records[0].fields.city
-        let state = response.records[0].fields.state
-        location.innerHTML = `${city}, ${state}`
+        let city = response.records[0].fields.city;
+        let state = response.records[0].fields.state;
+        location.innerHTML = `${city}, ${state}`;
         getWeather(coordinates);
     });
 }
 
-function printOutfit (temp, temp2, todayPrecip, tmrwPrecip) { 
-    let clothesToday= document.getElementById("todayClothes");
+function printOutfit(temp, temp2, todayPrecip, tmrwPrecip) {
+    let clothesToday = document.getElementById("todayClothes");
     let clothesTomorrow = document.getElementById("tomorrowClothes");
-    let todayPrecipPercentage = todayPrecip * 100
+    let todayPrecipPercentage = todayPrecip * 100;
 
-    var outfits = { 
-        hot:["images/shorts.png","images/whitetshirt.png", "images/dress.png"],
-        warm:["images/greytshirt.png","images/jeans.png", "images/dress.png", "images/sweatshirt.png", "images/shorts.png"],
-        chilly:["images/whitetshirt.png","images/sweatpants.png", "images/sweatshirt.png", "images/blackjacket.png", "images/dress.png", "images/jeans.png"],
-        cold:["images/greytshirt.png","images/jeans.png", "images/coat.png", "images/scarf.png", "images/blackjacket.png", "images/coat.png"], 
-        wintry:["images/freezing.png"] 
-    }; 
+    var outfits = {
+        hot: [
+            "images/shorts.png",
+            "images/whitetshirt.png",
+            "images/dress.png"
+        ],
+        warm: [
+            "images/greytshirt.png",
+            "images/jeans.png",
+            "images/dress.png",
+            "images/sweatshirt.png",
+            "images/shorts.png"
+        ],
+        chilly: [
+            "images/whitetshirt.png",
+            "images/sweatpants.png",
+            "images/sweatshirt.png",
+            "images/blackjacket.png",
+            "images/dress.png",
+            "images/jeans.png"
+        ],
+        cold: [
+            "images/greytshirt.png",
+            "images/jeans.png",
+            "images/coat.png",
+            "images/scarf.png",
+            "images/blackjacket.png",
+            "images/coat.png"
+        ],
+        wintry: ["images/freezing.png"]
+    };
 
-    let myOutfit = 0; 
+    let myOutfit = 0;
 
     if (temp >= 80) myOutfit = outfits.hot;
     if (temp >= 65 && temp < 80) myOutfit = outfits.warm;
@@ -87,36 +112,36 @@ function printOutfit (temp, temp2, todayPrecip, tmrwPrecip) {
     if (temp >= 25 && temp < 44) myOutfit = outfits.cold;
     if (temp < 25) myOutfit = outfits.wintry;
 
-        for (i = 0; i < myOutfit.length; i++) { 
-            newImg = document.createElement("img");
-            newImg.setAttribute("src", myOutfit[i]);
-            newImg.setAttribute("class", "round image is-128x128");
-            clothesToday.append(newImg);
-        }
-    
+    for (i = 0; i < myOutfit.length; i++) {
+        newImg = document.createElement("img");
+        newImg.setAttribute("src", myOutfit[i]);
+        newImg.setAttribute("class", "round image is-128x128");
+        clothesToday.append(newImg);
+    }
+
     if (todayPrecip > 0.35) {
         newImg = document.createElement("img");
         newImg.setAttribute("src", "images/umbrella.png");
         newImg.setAttribute("class", "round image is-128x128");
-        newPara = document.createElement("p")
-        newPara.innerHTML = `There is a ${todayPrecipPercentage}%  chance of rain today`
+        newPara = document.createElement("p");
+        newPara.innerHTML = `There is a ${todayPrecipPercentage}%  chance of rain today`;
         clothesToday.append(newImg);
         todayPrecipDiv.append(newPara);
     }
 
-    let myTomorrowOutfit = 0; 
+    let myTomorrowOutfit = 0;
     if (temp2 >= 80) myTomorrowOutfit = outfits.hot;
     if (temp2 >= 65 && temp2 < 80) myTomorrowOutfit = outfits.warm;
     if (temp2 >= 45 && temp2 < 64) myTomorrowOutfit = outfits.chilly;
     if (temp2 >= 25 && temp2 < 44) myTomorrowOutfit = outfits.cold;
-    if (temp2 < 25) myTomorrowOutfit = outfits.wintry;    
+    if (temp2 < 25) myTomorrowOutfit = outfits.wintry;
 
-        for (j = 0; j < myTomorrowOutfit.length; j++) { 
-            newImg2 = document.createElement("img");
-            newImg2.setAttribute("src", myTomorrowOutfit[j]);
-            newImg2.setAttribute("class", "round image is-128x128");
-            clothesTomorrow.append(newImg2);
-        }
+    for (j = 0; j < myTomorrowOutfit.length; j++) {
+        newImg2 = document.createElement("img");
+        newImg2.setAttribute("src", myTomorrowOutfit[j]);
+        newImg2.setAttribute("class", "round image is-128x128");
+        clothesTomorrow.append(newImg2);
+    }
 
     if (tmrwPrecip > 0.35) {
         newImg = document.createElement("img");
@@ -124,17 +149,17 @@ function printOutfit (temp, temp2, todayPrecip, tmrwPrecip) {
         newImg.setAttribute("class", "round image is-128x128");
         clothesTomorrow.append(newImg);
     }
-        return myOutfit;
+    return myOutfit;
 }
 
 function getWeather(coordinates) {
-    let tempHtml = document.getElementById("feelsLike")
+    let tempHtml = document.getElementById("feelsLike");
 
-    let tmrTempHigh = document.getElementById("tmrTempHigh")
-    let tmrTempLow = document.getElementById("tmrTempLow")
-    let tmrRain= document.getElementById("tmrRainChance")
+    let tmrTempHigh = document.getElementById("tmrTempHigh");
+    let tmrTempLow = document.getElementById("tmrTempLow");
+    let tmrRain = document.getElementById("tmrRainChance");
 
-    let hourlyPrecipProb = []
+    let hourlyPrecipProb = [];
 
     let lat = coordinates[1];
     let lon = coordinates[0];
@@ -143,34 +168,40 @@ function getWeather(coordinates) {
     get(url).then(function(response) {
         // Storing appropriate values
         let apparentTemp = response.currently.apparentTemperature;
-        let tmrwTempHighValue = response.daily.data[0].apparentTemperatureHigh
-        let tmrwTempLowValue = response.daily.data[0].apparentTemperatureLow
-        let tmrwRainChanceValue = response.daily.data[0].precipProbability * 100
-        let tmrwRainChanceDec = response.daily.data[0].precipProbability
+        let tmrwTempHighValue = response.daily.data[0].apparentTemperatureHigh;
+        let tmrwTempLowValue = response.daily.data[0].apparentTemperatureLow;
+        let tmrwRainChanceValue =
+            response.daily.data[0].precipProbability * 100;
+        let tmrwRainChanceDec = response.daily.data[0].precipProbability;
 
         // loop over every hours precip probability and store in an array
         response.hourly.data.forEach(element => {
             hourlyPrecipProb.push(element.precipProbability);
         });
-        
-        // grab max precip value from array 
-        let maxHourlyProb = Math.max(...hourlyPrecipProb)
+
+        // grab max precip value from array
+        let maxHourlyProb = Math.max(...hourlyPrecipProb);
 
         // Populate DOM with weather data
-        tempHtml.innerHTML = `${Math.round(apparentTemp)}&#176F`
-        tmrTempHigh.innerHTML = `${Math.round(tmrwTempHighValue)}&#176F`
-        tmrTempLow.innerHTML = `${Math.round(tmrwTempLowValue)}&#176F`
-        tmrRain.innerHTML = `${Math.round(tmrwRainChanceValue)}&#37;`
+        tempHtml.innerHTML = `${Math.round(apparentTemp)}&#176F`;
+        tmrTempHigh.innerHTML = `${Math.round(tmrwTempHighValue)}&#176F`;
+        tmrTempLow.innerHTML = `${Math.round(tmrwTempLowValue)}&#176F`;
+        tmrRain.innerHTML = `${Math.round(tmrwRainChanceValue)}&#37;`;
 
-        // Call printOutfit 
-        printOutfit(apparentTemp, tmrwTempLowValue, maxHourlyProb, tmrwRainChanceDec);
+        // Call printOutfit
+        printOutfit(
+            apparentTemp,
+            tmrwTempLowValue,
+            maxHourlyProb,
+            tmrwRainChanceDec
+        );
     });
 }
 
 function getIcon(zip) {
     let u = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=c7d69379e4f97e2b0e42695dddb0ad27`;
     const iconImg = document.getElementById("weatherIcon");
-    
+
     get(u)
         .then(response => response)
         .then(function(data) {
@@ -188,10 +219,8 @@ function getIcon(zip) {
 }
 
 // IIFE with default city Atlanta, GA
-(function(){
-    convertZipToCoordinates(30342)
-    getIcon(30342)
-    getDate()
+(function() {
+    convertZipToCoordinates(30342);
+    getIcon(30342);
+    getDate();
 })();
-
-
